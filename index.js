2d076dc0d5c64e642a1941f8c0906dd3a4203ed5
liorga <liorgal31892@gmail.com>
2021-05-11 13:45:13 +0300

    inittial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const data = require('./pokemons.json');
+
+
+
+for (const pokemon of data) {
+    const strId = `${pokemon.id}`.padStart(3, '0');
+    pokemon.img = `/images/${strId}.png`;
+    pokemon.counter = 0;
+}
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/pokemons/:id', (req, res) => {
+    const Id = req.params.id;
+    const searchRes = data.find((obj) => obj.id == Id);
+    searchRes.counter++;
+    res.sendFile(path.resolve('./pages/pokemonId.html'));
+})
+
+app.get("/api/pokemons/:id", (req, res) => {
+
+    const Id = req.params.id;
+    const searchRes = data.find((obj) => obj.id == Id);
+    res.send(searchRes);
+
+})
+
+app.get('/api/top3', (req, res) => {
+    data.sort((a, b) => {
+        return b.counter - a.counter;
+    });
+    const top3 = data.slice(0, 3);
+    res.send(top3);
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('./pages/index.html'));
+})
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
